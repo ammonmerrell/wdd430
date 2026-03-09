d89de774dc8f886afeace966092e0f4ef5d3f3c7
ammonmerrell <ammonmerrell@outlook.com>
2026-03-09 15:14:26 -0600

    imported state change from react

--- app/like-button.js
+++ app/like-button.js
@@ -1,11 +1,16 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function LikeButton() {
+    const [likes, setLikes] = useState(0);
+    
     function handleClick() {
             setLikes(likes + 1);
         }
 
         <button onClick={handleClick}>Like ({likes})</button>
 
-        const [likes, setLikes] = useState(0);
 
         return <button onClick={handleClick}>Like ({likes})</button>
 }
