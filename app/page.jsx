80ecccbfd8a03c3d889e35dba595f77bc08b9332
ammonmerrell <ammonmerrell@outlook.com>
2026-03-09 15:33:35 -0600

    removed redudant homepage function

--- app/page.jsx
+++ app/page.jsx
@@ -11,13 +11,12 @@ import LikeButton from './like-button';
        }
 
 
+
        function Header({ title }) {
         return <h1>{title ? title : 'Default title'}</h1>;
        }
        
        export default function HomePage() {
-        
-        function HomePage() {
             const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];    
             return (
                 <div>
@@ -31,4 +30,4 @@ import LikeButton from './like-button';
                 </div>
             );
         }
-       }   
+       
