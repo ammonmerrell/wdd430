81d4bbd9de79fbd9acfff8d5c426e5e21c8acac6
ammonmerrell <ammonmerrell@outlook.com>
2026-03-09 15:30:51 -0600

    added more spaces

--- app/page.jsx
+++ app/page.jsx
@@ -10,11 +10,13 @@ import LikeButton from './like-button';
         }
        }
 
+
        function Header({ title }) {
         return <h1>{title ? title : 'Default title'}</h1>;
        }
        
        export default function HomePage() {
+        
         function HomePage() {
             const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];    
             return (
